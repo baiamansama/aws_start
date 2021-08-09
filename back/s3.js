7b6ded3411bc320d4022cc47d4e389729c9fc234
Baiaman Bazarbaev <baiamansama@gmail.com>
2021-08-09 18:53:43 +0900

    first aws draft

--- back/s3.js
+++ back/s3.js
@@ -1,33 +1,32 @@
-import dotenv from 'dotenv'
 import aws from 'aws-sdk'
-import crypto from 'crypto'
-import { promisify } from "util"
-const randomBytes = promisify(crypto.randomBytes)
+import crypto, { randomBytes } from 'crypto'
+import { promisify } from 'util'
+import dotenv from 'dotenv'
 
 dotenv.config()
-
-const region = "us-west-2"
-const bucketName = "direct-upload-s3-bucket-thing"
+const region ="us-west-2"
+const bucketName = "aimon-upload-files"
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRETE_ACCESS_KEY
+
 
 const s3 = new aws.S3({
-  region,
-  accessKeyId,
-  secretAccessKey,
-  signatureVersion: 'v4'
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: "v4"
 })
 
 export async function generateUploadURL() {
-  const rawBytes = await randomBytes(16)
-  const imageName = rawBytes.toString('hex')
 
-  const params = ({
-    Bucket: bucketName,
-    Key: imageName,
-    Expires: 60
-  })
-  
-  const uploadURL = await s3.getSignedUrlPromise('putObject', params)
-  return uploadURL
+    const rawBytes = await randomBytes(16)
+    const imageName = rawBytes.toString('hex')
+    const params = ({
+        Bucket: bucketName, 
+        Key:imageName,
+        Expires: 60
+    })
+
+    const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+    return uploadURL
 }
--- back/server.js
+++ back/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { generateUploadURL } from './s3.js'
+import { generateUploadURL} from './s3.js'
 
 const app = express()
 
 app.use(express.static('front'))
 
-app.get('/s3Url', async (req, res) => {
-  const url = await generateUploadURL()
+app.get('/s3Url', async (req,res) =>{
+  const url = s3.generateUploadURL()
   res.send({url})
 })
 
